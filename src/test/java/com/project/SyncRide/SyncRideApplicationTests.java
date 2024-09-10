bd8f782df518b5e8a610b402397c266ccb4c4f43
Aldo Grabić <aldo.grabic99@gmail.com>
2024-09-10 12:07:59 +0200

    First commit

---
+++ src/test/java/com/project/SyncRide/SyncRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SyncRide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
