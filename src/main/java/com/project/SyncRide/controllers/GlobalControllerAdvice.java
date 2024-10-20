34e9b247b34d9e7a4512da103963c1cef1d1931f
Aldo Grabić <aldo.grabic99@gmail.com>
2024-10-20 11:43:59 +0200

    Manji ispravci dizajna u setupu, header popravljen i nadodan auth opcije

--- src/main/java/com/project/SyncRide/controllers/GlobalControllerAdvice.java
+++ src/main/java/com/project/SyncRide/controllers/GlobalControllerAdvice.java
@@ -4,8 +4,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.project.SyncRide.config.EmailNotVerifiedException;
 import com.project.SyncRide.models.user.User;
 import com.project.SyncRide.repositories.user.UserRepository;
 
@@ -37,4 +39,11 @@ public class GlobalControllerAdvice {
         String currentUri = request.getRequestURI();
         model.addAttribute("currentUri", currentUri);
     }
+
+    @ExceptionHandler(EmailNotVerifiedException.class)
+    public String handleEmailNotVerifiedException(Model model) {
+        model.addAttribute("error", "Prije prijave potvrdite svoju email adresu.");
+        return "login";
+    }
+
 }
